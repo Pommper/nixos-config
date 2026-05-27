36a59758f2e94e992564eda8b2bdb3205ca4dc3f
Pommper <michi.dyck06@gmail.com>
2026-05-27 04:30:54 +0200

    V 1.0.2

---
+++ modules/boot.nix
@@ -0,0 +1,13 @@
+# modules/boot.nix
+{ ... }:
+
+{
+# ── Bootloader (Grub statt systemd-boot) ─────────────────────
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.grub = {
+    enable  = true;
+    device  = "nodev";
+    efiSupport = true;
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
+}
